ec0642719212d235670007a5c2b481b457b119ed
Timothy Goeke Dee <tgdee123@gmail.com>
2022-03-05 15:25:37 -0500

    resume upload

--- ResumeUpload.aspx.cs
+++ ResumeUpload.aspx.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Lab3
 {
@@ -39,6 +42,33 @@ namespace Lab3
                         FileUpload1.SaveAs(Server.MapPath("~/Resumes/" + FileUpload1.FileName));
                         lblMessage.Text = "Resume Uploaded";
                         lblMessage.ForeColor = System.Drawing.Color.Green;
+
+                        var connectionFromConfiguration = WebConfigurationManager.ConnectionStrings["Lab3"];
+
+                        SqlConnection dbConnection = new SqlConnection(connectionFromConfiguration.ConnectionString);
+
+                        try
+                        {
+                            string insertString = "INSERT INTO Resume (FileName, FileLocation) VALUES (@fileName, @fileLocation)";
+                            dbConnection.Open();
+
+                            using (SqlCommand cmd = new SqlCommand(insertString, dbConnection))
+                            {
+                                cmd.Parameters.Add("@fileName", SqlDbType.NVarChar, 50).Value = FileUpload1.FileName.ToString();
+                                cmd.Parameters.Add("@fileLocation", SqlDbType.NVarChar, 50).Value = "~/Resumes/" + FileUpload1.FileName;
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                        catch (SqlException ex)
+                        {
+                            lblMessage.Text = ex.Message;
+                        }
+                        finally
+                        {
+                            dbConnection.Close();
+                            dbConnection.Dispose();
+                        }
+                        
                     }
 
                     
