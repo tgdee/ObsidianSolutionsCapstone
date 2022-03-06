0fa04097e763a8a469e62bbd0cf377277225484c
Shane McCarthy <mccartsf@dukes.jmu.edu>
2022-03-05 19:00:04 -0500

    resume on the site master

--- Student.aspx.designer.cs
+++ Student.aspx.designer.cs
@@ -159,6 +159,15 @@ namespace Lab3
         protected global::System.Web.UI.WebControls.Button btnSearch;
 
         /// <summary>
+        /// LblFile control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblFile;
+
+        /// <summary>
         /// gvSearch control.
         /// </summary>
         /// <remarks>
