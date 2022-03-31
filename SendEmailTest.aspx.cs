ace5f7132217b721010718db734989b98ff3fe06
Timothy Goeke Dee <tgdee123@gmail.com>
2022-03-31 12:16:05 -0400

    created pages for automatically sending emails

---
+++ SendEmailTest.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab3
+{
+    public partial class SendEmailTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            Logging.SendEmail(txtEmailBody.Text);
+        }
+    }
+}
