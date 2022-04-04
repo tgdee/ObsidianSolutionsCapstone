2c23b08084bba4c5aa38801778c026adf4b14ef6
eagenH <eagenhayes@gmail.com>
2022-04-04 18:19:47 -0400

    com

--- Account.aspx.designer.cs
+++ Account.aspx.designer.cs
@@ -87,15 +87,6 @@ namespace Lab3
         protected global::System.Web.UI.WebControls.Button btnDelete;
 
         /// <summary>
-        /// btnBio control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnBio;
-
-        /// <summary>
         /// sqltesting control.
         /// </summary>
         /// <remarks>
