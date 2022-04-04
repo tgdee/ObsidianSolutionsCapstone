a0f95b70ac4d7c1531526251ff1916ecbc1143a8
Shane McCarthy <mccartsf@dukes.jmu.edu>
2022-04-04 18:21:39 -0400

    changes to something

--- StudentMaster.Master.designer.cs
+++ StudentMaster.Master.designer.cs
@@ -49,5 +49,14 @@ namespace Lab3
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+
+        /// <summary>
+        /// lblUserStat control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblUserStat;
     }
 }
