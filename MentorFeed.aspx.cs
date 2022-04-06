9dcc3276017aee5e2292da978f7eafb129e23e65
eagenH <eagenhayes@gmail.com>
2022-04-06 16:01:45 -0400

    com

--- MentorFeed.aspx.cs
+++ MentorFeed.aspx.cs
@@ -12,6 +12,7 @@ namespace Lab3
 {
     public partial class MentorFeed : System.Web.UI.Page
     {
+        readonly SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["Lab3"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
