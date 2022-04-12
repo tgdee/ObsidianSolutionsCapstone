fe8fce6f09868eaf0b03fe8b7ca5ccff962bb9ea
Shane McCarthy <mccartsf@dukes.jmu.edu>
2022-04-12 08:02:21 -0400

    changed the master pages and restrictions to allow certain accounts access

--- MemberTest.aspx.cs
+++ MemberTest.aspx.cs
@@ -26,47 +26,47 @@ namespace Lab3
         }
 
 
-        protected void btnAddRow_Click(object sender, EventArgs e)
-        {
-            var connectionFromConfiguration = WebConfigurationManager.ConnectionStrings["Lab3"];
+        //protected void btnAddRow_Click(object sender, EventArgs e)
+        //{
+        //    var connectionFromConfiguration = WebConfigurationManager.ConnectionStrings["Lab3"];
 
-            using (SqlConnection dbConnection = new SqlConnection(connectionFromConfiguration.ConnectionString))
-            {
-                try
-                {
-                    string insertString = "INSERT INTO Member (FirstName, LastName, Email) VALUES (@firstName, @lastName, @email)";
+        //    using (SqlConnection dbConnection = new SqlConnection(connectionFromConfiguration.ConnectionString))
+        //    {
+        //        try
+        //        {
+        //            string insertString = "INSERT INTO Member (FirstName, LastName, Email) VALUES (@firstName, @lastName, @email)";
 
-                    dbConnection.Open();
+        //            dbConnection.Open();
 
-                    using (SqlCommand cmd = new SqlCommand(insertString, dbConnection))
-                    {
-                        string firstName = txtMemberFirstName.Text;
-                        string lastName = txtMemberLastName.Text;
-                        string email = txtMemberEmail.Text;
+        //            using (SqlCommand cmd = new SqlCommand(insertString, dbConnection))
+        //            {
+        //                string firstName = txtMemberFirstName.Text;
+        //                string lastName = txtMemberLastName.Text;
+        //                string email = txtMemberEmail.Text;
 
-                        cmd.Parameters.Add("@firstName", SqlDbType.NVarChar, 50).Value = firstName;
-                        cmd.Parameters.Add("@lastName", SqlDbType.NVarChar, 50).Value = lastName;
-                        cmd.Parameters.Add("@email", SqlDbType.NVarChar, 50).Value = email;
+        //                cmd.Parameters.Add("@firstName", SqlDbType.NVarChar, 50).Value = firstName;
+        //                cmd.Parameters.Add("@lastName", SqlDbType.NVarChar, 50).Value = lastName;
+        //                cmd.Parameters.Add("@email", SqlDbType.NVarChar, 50).Value = email;
 
-                        cmd.ExecuteNonQuery();
+        //                cmd.ExecuteNonQuery();
 
-                        Response.Redirect("~/MemberTest.aspx");
+        //                Response.Redirect("~/MemberTest.aspx");
 
-                    }
+        //            }
 
-                }
-                catch (SqlException ex)
-                {
-                    ltError.Text = ex.Message;
-                }
-                finally
-                {
-                    dbConnection.Close();
-                    dbConnection.Dispose();
-                }
-            }
+        //        }
+        //        catch (SqlException ex)
+        //        {
+        //            ltError.Text = ex.Message;
+        //        }
+        //        finally
+        //        {
+        //            dbConnection.Close();
+        //            dbConnection.Dispose();
+        //        }
+        //    }
 
-        }
+        //}
 
         protected void gvMember_SelectedIndexChanged(object sender, EventArgs e)
         {
