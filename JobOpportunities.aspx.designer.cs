49c351ea7abfec890289c0c932160df3a1d71144
kwcaldwell <100966495+kwcaldwell@users.noreply.github.com>
2022-04-13 16:50:31 -0400

    feed updates

--- JobOpportunities.aspx.designer.cs
+++ JobOpportunities.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace Lab3
         protected global::System.Web.UI.WebControls.Label lblError;
 
         /// <summary>
+        /// gvOpportunity control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvOpportunity;
+
+        /// <summary>
+        /// lblMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+
+        /// <summary>
         /// lvStudentOpportunities control.
         /// </summary>
         /// <remarks>
@@ -31,14 +49,5 @@ namespace Lab3
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListView lvStudentOpportunities;
-
-        /// <summary>
-        /// lvAnnouncements control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView lvAnnouncements;
     }
 }
